a17cf7bb412f98a5b2ba1b8ea5e29f3107798a12
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2017-11-17 17:13:23 +0100

    gc_sync_ffs: use unique sync chain register name so that a simple constraint can cover all false paths through clock synchronizers

--- modules/common/gc_sync_ffs.vhd
+++ modules/common/gc_sync_ffs.vhd
@@ -6,7 +6,7 @@
 -- Author     : Tomasz Wlostowski
 -- Company    : CERN BE-Co-HT
 -- Created    : 2010-06-14
--- Last update: 2017-02-03
+-- Last update: 2017-11-16
 -- Platform   : FPGA-generic
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -60,22 +60,22 @@ end gc_sync_ffs;
 -- altera message_off 10335
 
 architecture behavioral of gc_sync_ffs is
-  signal sync0, sync1, sync2 : std_logic;
+  signal gc_sync_ffs_sync0, gc_sync_ffs_sync1, gc_sync_ffs_sync2 : std_logic;
 
   attribute shreg_extract : string;
-  attribute shreg_extract of sync0  : signal is "no";
-  attribute shreg_extract of sync1  : signal is "no";
-  attribute shreg_extract of sync2  : signal is "no";
+  attribute shreg_extract of gc_sync_ffs_sync0  : signal is "no";
+  attribute shreg_extract of gc_sync_ffs_sync1  : signal is "no";
+  attribute shreg_extract of gc_sync_ffs_sync2  : signal is "no";
 
   attribute keep : string;
-  attribute keep of sync0  : signal is "true";
-  attribute keep of sync1  : signal is "true";
+  attribute keep of gc_sync_ffs_sync0  : signal is "true";
+  attribute keep of gc_sync_ffs_sync1  : signal is "true";
 
   -- synchronizer attribute for Vivado
   attribute ASYNC_REG : string;
-  attribute ASYNC_REG of sync0 : signal is "true";
-  attribute ASYNC_REG of sync1 : signal is "true";
-  attribute ASYNC_REG of sync2 : signal is "true";
+  attribute ASYNC_REG of gc_sync_ffs_sync0 : signal is "true";
+  attribute ASYNC_REG of gc_sync_ffs_sync1 : signal is "true";
+  attribute ASYNC_REG of gc_sync_ffs_sync2 : signal is "true";
 
 begin
 
@@ -84,19 +84,19 @@ begin
     process(clk_i, rst_n_i)
     begin
       if(rst_n_i = '0') then
-        sync0    <= '0';
-        sync1    <= '0';
-        sync2    <= '0';
+        gc_sync_ffs_sync0    <= '0';
+        gc_sync_ffs_sync1    <= '0';
+        gc_sync_ffs_sync2    <= '0';
         synced_o <= '0';
         npulse_o <= '0';
         ppulse_o <= '0';
       elsif rising_edge(clk_i) then
-        sync0    <= data_i;
-        sync1    <= sync0;
-        sync2    <= sync1;
-        synced_o <= sync1;
-        npulse_o <= sync2 and not sync1;
-        ppulse_o <= not sync2 and sync1;
+        gc_sync_ffs_sync0    <= data_i;
+        gc_sync_ffs_sync1    <= gc_sync_ffs_sync0;
+        gc_sync_ffs_sync2    <= gc_sync_ffs_sync1;
+        synced_o <= gc_sync_ffs_sync1;
+        npulse_o <= gc_sync_ffs_sync2 and not gc_sync_ffs_sync1;
+        ppulse_o <= not gc_sync_ffs_sync2 and gc_sync_ffs_sync1;
       end if;
     end process;
   end generate sync_posedge;
@@ -105,19 +105,19 @@ begin
     process(clk_i, rst_n_i)
     begin
       if(rst_n_i = '0') then
-        sync0    <= '0';
-        sync1    <= '0';
-        sync2    <= '0';
+        gc_sync_ffs_sync0    <= '0';
+        gc_sync_ffs_sync1    <= '0';
+        gc_sync_ffs_sync2    <= '0';
         synced_o <= '0';
         npulse_o <= '0';
         ppulse_o <= '0';
       elsif falling_edge(clk_i) then
-        sync0    <= data_i;
-        sync1    <= sync0;
-        sync2    <= sync1;
-        synced_o <= sync1;
-        npulse_o <= sync2 and not sync1;
-        ppulse_o <= not sync2 and sync1;
+        gc_sync_ffs_sync0    <= data_i;
+        gc_sync_ffs_sync1    <= gc_sync_ffs_sync0;
+        gc_sync_ffs_sync2    <= gc_sync_ffs_sync1;
+        synced_o <= gc_sync_ffs_sync1;
+        npulse_o <= gc_sync_ffs_sync2 and not gc_sync_ffs_sync1;
+        ppulse_o <= not gc_sync_ffs_sync2 and gc_sync_ffs_sync1;
       end if;
     end process;
   end generate sync_negedge;
