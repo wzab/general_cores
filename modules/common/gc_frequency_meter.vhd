c896dee8996dbe4eaae5c9a63431a0d093be32ee
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2018-03-16 12:18:49 +0100

    gc_frequency_meter: make sure it works with no input clock

--- modules/common/gc_frequency_meter.vhd
+++ modules/common/gc_frequency_meter.vhd
@@ -10,7 +10,8 @@ entity gc_frequency_meter is
   generic(
     g_with_internal_timebase : boolean := true;
     g_clk_sys_freq           : integer;
-    g_counter_bits           : integer := 32);
+    g_counter_bits           : integer := 32;
+    g_prescaler              : integer := 4);
 
   port(
     clk_sys_i    : in  std_logic;
@@ -32,6 +33,9 @@ architecture behavioral of gc_frequency_meter is
   signal cntr_meas : unsigned(g_counter_bits-1 downto 0);
   signal freq_reg  : std_logic_vector(g_counter_bits-1 downto 0);
 
+  signal prescaler_cnt : unsigned(3 downto 0);
+  signal prescaled_in  : std_logic;
+  signal tick : std_logic;
 begin
 
   gen_internal_timebase : if(g_with_internal_timebase = true) generate
@@ -54,42 +58,55 @@ begin
       end if;
     end process;
 
-  U_Sync_Gate : gc_pulse_synchronizer
-    port map (
-      clk_in_i  => clk_sys_i,
-      clk_out_i => clk_in_i,
-      rst_n_i   => rst_n_i,
-      d_ready_o => freq_valid_o,
-      d_p_i     => gate_pulse,
-      q_p_o     => gate_pulse_synced);
+    gate_pulse_synced <= gate_pulse;
 
   end generate gen_internal_timebase;
 
   gen_external_timebase : if(g_with_internal_timebase = false) generate
 
-    U_Sync_Gate : gc_pulse_synchronizer
-    port map (
-      clk_in_i  => clk_sys_i,
-      clk_out_i => clk_in_i,
-      rst_n_i   => rst_n_i,
-      d_ready_o => freq_valid_o,
-      d_p_i     => pps_p1_i,
-      q_p_o     => gate_pulse_synced);
-
+    U_Sync_Gate : gc_sync_ffs
+      port map (
+        clk_i  => clk_sys_i,
+        rst_n_i   => rst_n_i,
+        data_i     => pps_p1_i,
+        ppulse_o     => gate_pulse_synced);
   end generate gen_external_timebase;
 
 
-  p_freq_counter : process (clk_in_i, rst_n_i)
+  p_prescaler : process(clk_in_i, rst_n_i)
+  begin
+    if rst_n_i = '0' then
+      prescaler_cnt <= (others => '0');
+      prescaled_in  <= '0';
+    elsif rising_edge(clk_in_i) then
+      if(prescaler_cnt = g_prescaler-1) then
+        prescaler_cnt <= (others => '0');
+        prescaled_in  <= not prescaled_in;
+      else
+        prescaler_cnt <= prescaler_cnt + 1;
+      end if;
+    end if;
+  end process;
+
+
+  U_Sample_Clock : gc_sync_ffs
+    port map (
+      clk_i  => clk_sys_i,
+      rst_n_i   => rst_n_i,
+      data_i     => prescaled_in,
+      ppulse_o     => tick);
+  
+  p_freq_counter : process (clk_sys_i, rst_n_i)
   begin
     if rst_n_i = '0' then               -- asynchronous reset (active low)
       cntr_meas <= (others => '0');
       freq_reg  <= (others => '0');
     elsif rising_edge(clk_in_i) then
-      
+
       if(gate_pulse_synced = '1') then
         freq_reg  <= std_logic_vector(cntr_meas);
         cntr_meas <= (others => '0');
-      else
+      elsif (tick = '1') then
         cntr_meas <= cntr_meas + 1;
       end if;
     end if;
